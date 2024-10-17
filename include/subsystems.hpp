32d2a92472e45977fe1044b3bf02ef1354c0224b
Aud4city-Pr0 <166267444+Aud4city-Pr0@users.noreply.github.com>
2024-10-17 15:24:35 -0400

    Update subsystems.hpp

--- include/subsystems.hpp
+++ include/subsystems.hpp
@@ -6,5 +6,6 @@
 // Your motors, sensors, etc. should go here.  Below are examples
 
 // Our bot systems and mechs
-inline pros::Motor IntakeMotor(11);
-inline pros::adi::Pneumatics MogoMech('a', false);
+inline pros::Motor IntakeMotor(11, pros::v5::MotorGears::green);
+inline pros::adi::Pneumatics MogoMech('A', false);
+inline pros::Motor LadyBrownMech(15, pros::v5::MotorGears::red);
